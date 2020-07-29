86882327bdb4c7f04e8219fac5affa15432cf059
0kk470 <2549366978@qq.com>
2020-07-29 18:03:51 +0800

    update light map

---
+++ HelloLightMap.cpp
@@ -0,0 +1,165 @@
+#include "HelloLightMap.h"
+
+void HelloLightMap::OnInit()
+{
+	glEnable(GL_DEPTH_TEST);
+	//Default Shader
+	m_LightingObjShader = new Shader("./Shaders/Vertex/HelloLightMap/Cube.vertex", "./Shaders/Fragment/HelloLightMap/Cube.frag");
+	m_LampShader = new Shader("./Shaders/Vertex/HelloLightMap/Light.vertex", "./Shaders/Fragment/HelloLightMap/Light.frag");
+
+
+	glGenVertexArrays(1, &containerVAO);
+	glGenBuffers(1, &VBO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindVertexArray(containerVAO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(GL_FLOAT) * 6, (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(GL_FLOAT) * 6, (GLvoid*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+	glBindVertexArray(0);
+
+	glGenVertexArrays(1, &lightVAO);
+	glBindVertexArray(lightVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(GL_FLOAT) * 6, (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	glBindVertexArray(0);
+}
+
+void HelloLightMap::OnRender()
+{
+	glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+	DrawLightParamWindow();
+
+	auto mainCamera = Camera::GetMainCamera();
+	m_LightingObjShader->Use();
+	m_LightingObjShader->setVec3("light.position", lightPos[0], lightPos[1], lightPos[2]);
+	m_LightingObjShader->setVec3("viewPos", mainCamera->Position);
+
+	m_LightingObjShader->setVec3("light.ambient", light_ambient[0], light_ambient[1], light_ambient[2]);
+	m_LightingObjShader->setVec3("light.diffuse", light_diffuse[0], light_diffuse[1], light_diffuse[2]);
+	m_LightingObjShader->setVec3("light.specular", light_specular[0], light_specular[1], light_specular[2]);
+
+	m_LightingObjShader->setVec3("material.ambient", ambient[0], ambient[1], ambient[2]);
+	m_LightingObjShader->setVec3("material.diffuse", diffuse[0], diffuse[1], diffuse[2]);
+	m_LightingObjShader->setVec3("material.specular", specular[0], specular[1], specular[2]);
+	m_LightingObjShader->setFloat("material.shininess", shininess);
+
+	glm::mat4 view = mainCamera->GetViewMatrix();
+	auto projection = glm::perspective(mainCamera->Zoom, 800.0f / 600.0f, 0.1f, 100.0f);
+
+	glm::mat4 model(1);
+	m_LightingObjShader->setMat4("model", model);
+	m_LightingObjShader->setMat4("view", view);
+	m_LightingObjShader->setMat4("projection", projection);
+
+
+	glBindVertexArray(containerVAO);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+	glBindVertexArray(0);
+
+	m_LampShader->Use();
+
+	m_LampShader->setMat4("projection", projection);
+	m_LampShader->setMat4("view", view);
+	model = glm::mat4(1);
+	model = glm::translate(model, glm::vec3(lightPos[0], lightPos[1], lightPos[2]));
+	model = glm::scale(model, glm::vec3(0.2f));
+
+	m_LampShader->setMat4("model", model);
+	m_LampShader->setVec3("lightColor", lightColor[0], lightColor[1], lightColor[2]);
+	glBindVertexArray(lightVAO);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+	glBindVertexArray(0);
+}
+
+void HelloLightMap::OnWindowAttach(GLFWwindow* wnd)
+{
+}
+
+void HelloLightMap::HandleInput(GLFWwindow* wnd)
+{
+	GLfloat deltaTime = Time::deltaTime;
+	auto mainCamera = Camera::GetMainCamera();
+	if (glfwGetKey(wnd, GLFW_KEY_W))
+	{
+		mainCamera->ProcessKeyboard(FORWARD, deltaTime);
+	}
+	else if (glfwGetKey(wnd, GLFW_KEY_S))
+	{
+		mainCamera->ProcessKeyboard(BACKWARD, deltaTime);
+	}
+	if (glfwGetKey(wnd, GLFW_KEY_A))
+	{
+		mainCamera->ProcessKeyboard(LEFT, deltaTime);
+	}
+	else if (glfwGetKey(wnd, GLFW_KEY_D))
+	{
+		mainCamera->ProcessKeyboard(RIGHT, deltaTime);
+	}
+}
+
+void HelloLightMap::OnMouseMoveCallback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	GLfloat xoffset = xpos - lastX;
+	GLfloat yoffset = lastY - ypos;  // Reversed since y-coordinates go from bottom to left
+
+	lastX = xpos;
+	lastY = ypos;
+
+	Camera::GetMainCamera()->ProcessMouseMovement(xoffset, yoffset);
+}
+
+void HelloLightMap::OnMouseScrollCallBack(GLFWwindow* window, double xoffset, double yoffset)
+{
+	Camera::GetMainCamera()->ProcessMouseScroll(yoffset);
+}
+
+void HelloLightMap::OnDeInit()
+{
+	delete m_LightingObjShader;
+	delete m_LampShader;
+}
+
+void HelloLightMap::DrawLightParamWindow()
+{
+	ImGui::SetNextWindowPos(ImVec2(800, 600), 0, ImVec2(1, 1));
+	ImGui::Begin("Light Modifier", 0, ImGuiWindowFlags_AlwaysAutoResize);
+	ImGui::BulletText("Lamp Attribute");
+	ImGui::ColorEdit3("Lamp Color", lightColor);
+	ImGui::DragFloat3("Lamp Pos", lightPos, 0.05f);
+	ImGui::BulletText("Cube Attribute");
+	ImGui::DragFloat3("Ambient ", ambient, 0.05f, 0, 1);
+	ImGui::DragFloat3("Diffuse ", diffuse, 0.05f, 0, 1);
+	ImGui::DragFloat3("Specular ", specular, 0.05f, 0, 1);
+	ImGui::SliderInt("shininess", &shininess, 0, 256);
+	ImGui::BulletText("Light Attribute");
+	ImGui::DragFloat3("Ambient ", light_ambient, 0.05f, 0, 1);
+	ImGui::DragFloat3("Diffuse ", light_diffuse, 0.05f, 0, 1);
+	ImGui::DragFloat3("Specular ", light_specular, 0.05f, 0, 1);
+	ImGui::End();
+}
+
+void HelloLightMap::basic_light_exercise3()
+{
+	m_LightingObjShader = new Shader("./Shaders/Vertex/HelloLightMap/basic_light_exercise3.vertex", "./Shaders/Fragment/HelloLightMap/basic_light_exercise3.frag");
+	m_LampShader = new Shader("./Shaders/Vertex/HelloLightMap/lamp.vertex", "./Shaders/Fragment/HelloLightMap/lamp.frag");
+}
+
+void HelloLightMap::basic_light_exercise4()
+{
+	m_LightingObjShader = new Shader("./Shaders/Vertex/HelloLightMap/basic_light_exercise4.vertex", "./Shaders/Fragment/HelloLightMap/basic_light_exercise4.frag");
+	m_LampShader = new Shader("./Shaders/Vertex/HelloLightMap/lamp.vertex", "./Shaders/Fragment/HelloLightMap/lamp.frag");
+}
---
+++ HelloLightMap.h
@@ -0,0 +1,99 @@
+#pragma once
+#include "Painter.h"
+class HelloLightMap : public Painter
+{
+
+public:
+    void OnInit() override;
+
+    void OnRender() override;
+
+    void OnWindowAttach(GLFWwindow* wnd) override;
+
+    void HandleInput(GLFWwindow* wnd) override;
+
+    void  OnMouseMoveCallback(GLFWwindow* window, double xpos, double ypos) override;
+
+    void  OnMouseScrollCallBack(GLFWwindow* window, double xoffset, double yoffset) override;
+
+    void OnDeInit() override;
+
+private:
+
+    void DrawLightParamWindow();
+
+    void basic_light_exercise3();
+
+    void basic_light_exercise4();
+
+    Shader* m_LightingObjShader, * m_LampShader;
+
+    GLuint VBO, containerVAO, lightVAO;
+
+    float vertices[6 * 6 * 6] = {
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+
+         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
+    };
+
+    bool firstMouse = true;
+
+    GLfloat lastX, lastY;
+
+    float lightColor[3] = { 1.0f, 1.0f, 1.0f };
+
+    float lightPos[3] = { 0.5f, 0.8f, 1.0f };
+
+    float ambient[3] = { 1.0f, 0.5f, 0.31f };
+
+    float diffuse[3] = { 1.0f, 0.5f, 0.31f };
+
+    float specular[3] = { 0.5f, 0.5f, 0.5f };
+
+    float light_ambient[3] = { 0.2f, 0.2f, 0.2f };
+
+    float light_diffuse[3] = { 0.5f, 0.5f, 0.5f };
+
+    float light_specular[3] = { 1.0f, 1.0f, 1.0f };
+
+    int shininess = 32;
+};
+
